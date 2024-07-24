b95622de7f1c953a3d8e703af18191c292871e4f
Ubuntu <ubuntu@ip-172-31-25-2.ec2.internal>
2024-07-24 02:49:43 +0000

    seen

--- install.sh
+++ install.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 # Install dependencies
 sudo apt update
-sudo apt install -y net-tools docker.io nginx
+sudo apt install -y net-tools docker.io nginx logrotate
 
 # Copy main script to /usr/local/bin
 sudo cp devopsfetch.sh /usr/local/bin/devopsfetch
 sudo chmod +x /usr/local/bin/devopsfetch
-
-
 
 # Setup systemd service
 sudo cp devopsfetch.service /etc/systemd/system/
@@ -15,6 +13,27 @@ sudo systemctl daemon-reload
 sudo systemctl enable devopsfetch.service
 sudo systemctl start devopsfetch.service
 
+# Create logrotate configuration
+sudo tee /etc/logrotate.d/devopsfetch > /dev/null <<EOL
+/var/log/devopsfetch.log {
+    daily
+    rotate 7
+    compress
+    missingok
+    notifempty
+    create 644 root root
+    sharedscripts
+    postrotate
+        systemctl reload devopsfetch.service > /dev/null 2>/dev/null || true
+    endscript
+    su root root
+}
+EOL
+
+# Ensure the log file exists and has the correct permissions
+sudo touch /var/log/devopsfetch.log
+sudo chown root:root /var/log/devopsfetch.log
+sudo chmod 644 /var/log/devopsfetch.log
 
 echo "DevOpsFetch has been installed and the service has been started."
 
